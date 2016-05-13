83e2e9f5c337a5e8a7cb547afb91a35801ffdb3a
Xinzy <shaozengy@gmail.com>
2016-05-13 17:53:40 +0800

    修复错误的包名

--- okhttp3/src/androidTest/java/com/xinzy/okhttp3/ApplicationTest.java
+++ okhttp3/src/androidTest/java/com/xinzy/okhttp3/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.xinzy.http3;
+package com.xinzy.okhttp3;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- okhttp3/src/test/java/com/xinzy/okhttp3/ExampleUnitTest.java
+++ okhttp3/src/test/java/com/xinzy/okhttp3/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.xinzy.http3;
+package com.xinzy.okhttp3;
 
 import org.junit.Test;
 
